d05841005f88df3a989d14eb643dd52ed4c5ce56
sskiragu <skiragu18@gmail.com>
2021-01-19 12:37:40 +0300

    Updates

---
+++ precache-manifest.58213457ad530cf79ee2fe29dab75d5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b379d94c3dcddea9eab33dec3db182c",
+    "url": "/react-form-validation/index.html"
+  },
+  {
+    "revision": "5e66a110be143dd7820f",
+    "url": "/react-form-validation/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "28847a82ca9ba1e5581a",
+    "url": "/react-form-validation/static/css/main.73a371da.chunk.css"
+  },
+  {
+    "revision": "5e66a110be143dd7820f",
+    "url": "/react-form-validation/static/js/2.5decc0c5.chunk.js"
+  },
+  {
+    "revision": "28847a82ca9ba1e5581a",
+    "url": "/react-form-validation/static/js/main.70684413.chunk.js"
+  },
+  {
+    "revision": "ca3af79ccf1d0de84c0c",
+    "url": "/react-form-validation/static/js/runtime-main.9f17599b.js"
+  }
+]);
